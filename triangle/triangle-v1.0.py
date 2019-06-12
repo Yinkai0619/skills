ac60156b253f3e3ff1af92ac36688da8237d9cf4
Li Yinkai <yinkai.li@foxmail.com>
2019-06-12 17:34:09 +0800

    Update triangle

--- triangle/triangle-v1.0.py
+++ triangle/triangle-v1.0.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-triangle = [[1],[1,1]]
+triangle = [[1],[1,1]]  # 定义前两行
 n = 6
-for i in range(2,n):
-    pre = triangle[i-1]
-    cur = [1]
+for i in range(2,n):  # 后四行
+    pre = triangle[i-1]     # 前一行
+    cur = [1]       #当前行
     for j in range(0,i-1):
         cur.append(pre[j] + pre[j + 1])
     cur.append(1)
---
+++ triangle/triangle-v1.1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# triangle = [[1],[1,1]]  # 定义前两行
+# n = 6
+# for i in range(2,n):  # 后四行
+#     pre = triangle[i-1]     # 前一行
+#     cur = [1]       #当前行
+#     for j in range(0,i-1):
+#         cur.append(pre[j] + pre[j + 1])
+#     cur.append(1)
+#     triangle.append(cur)
+# print(triangle)
+
+triangle = []
+n = 6
+for i in range(n):
+    cur = [1]
+    triangle.append(cur)
+    if i == 0:
+        continue
+    pre = triangle(i-1)
+    for j in range(len())
+
