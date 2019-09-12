a24c2ecc0e4e24d33ab74187bca62953ee627d97
Li Yinkai <yinkai.li@foxmail.com>
2019-09-12 16:30:24 +0800

    Add Sorts/Selection_sorts/Heapsort/heapsort-v1.0.py

---
+++ Sorts/Selection_sorts/Heapsort/heapsort-v1.0.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+'''
+Author: Li Yinkai
+Email: yinkai.li@foxmail.com
+Blog: https://blog.51cto.com/yinkai
+
+Date: 2019/9/12 10:11
+'''
+
+import math
+
+def print_tree(array):
+    '''
+
+    :param array:
+    :return:
+    '''
+    '''
+        i   前空格         元素间
+    1   3   7=2*3-1     0=2*4+1
+    2   2   3=2*2-1     7=2*3+1
+    3   1   1=2*1-1     3=2*1+1
+    4   0   0=2*0-1     1=2*0+1
+    '''
+
+
+
