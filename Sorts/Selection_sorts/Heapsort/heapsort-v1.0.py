17c21eec8bbed4333a5d86b6c65bbd74f6a0dfc4
Li Yinkai <yinkai.li@foxmail.com>
2019-09-24 17:55:19 +0800

    Amend count_words-v2.0.py

--- Sorts/Selection_sorts/Heapsort/heapsort-v1.0.py
+++ Sorts/Selection_sorts/Heapsort/heapsort-v1.0.py
@@ -9,20 +9,33 @@ Date: 2019/9/12 10:11
 '''
 
 import math
+import random
+import print_tree
 
-def print_tree(array):
+
+'''
+核心算法：
+    对于堆排序的核心算法就是堆结点的调整
+    1. 度数为2的结点A，如果它的左右孩子结点的最大值比它大，则将这个最大值和该结点交换；
+    2. 度数为1的结点A，如果它的左孩子的值大于它，则交换；
+    3. 如果结点A被交换到新的位置，还需要和其孩子结点重复上面的过程。
+'''
+
+# 构建待排序元素：
+# origin = [x * 10 for x in range(1, 10)]
+# random.shuffle(origin)
+# origin.insert(0, 0)
+origin = [0, 10, 20, 90, 80, 50, 60, 40, 30, 70]    # 为了能和编码对应，增加一个无用的0在首位
+print(origin)
+print_tree.print_tree(origin, True)
+
+total = len(origin) - 1
+
+def heap_adjust(n, i, array: list):
     '''
-
+    调整当前结点（核心算法）
+    :param n:
+    :param i:
     :param array:
     :return:
     '''
-    '''
-        i   前空格         元素间
-    1   3   7=2*3-1     0=2*4+1
-    2   2   3=2*2-1     7=2*3+1
-    3   1   1=2*1-1     3=2*1+1
-    4   0   0=2*0-1     1=2*0+1
-    '''
-
-
-
--- Sorts/Selection_sorts/Heapsort/print_tree.py
+++ Sorts/Selection_sorts/Heapsort/print_tree.py
@@ -18,71 +18,74 @@ Date: 2019/9/7 下午4:43
 
 '''
 
-import math
+def print_tree(array: list, supplement=True):
+    import math
+
+    def print_tree1(array):
+        '''
+        居中对齐方案, 逐个打印array中的元素
+        :param array:
+        :return:
+        '''
+        unit_width = len(str(array[-1]))  # 取最大元素的宽度作为单位宽度
+        length = len(array)
+        depth = math.ceil(math.log2(length + 1))  # 二叉树的深度（二叉树性质4）：具有n个结点的完全二叉树的深度为: math.ceil(math.log2(n))+1
+        index = 0
+        width = 2 ** depth - 1  # 满二叉树的宽度（作为第一行的宽度数居中打印元素）。
+        # 从上往下用投影的方法观察満二叉树时，各栅格（结点位置）互不重叠，且栅格数量等于满二叉树的结点总数。
+        # 满二叉树的结点数量为（二叉树性质2）: 2 ** depth(二叉树的深度) - 1
+
+        for i in range(depth):  # 二叉树的行
+            for j in range(2 ** i):  # 计数：j来控制为每行元素的个数。0:1; 1:0,1; 2:0,1,2,3; 3:0,1,2,3,4,5,6,7
+                print('{:^{}}'.format(array[index], width * unit_width), end=' ' * unit_width)  # 居中打印元素，最后补充一个单位数空格
+                index += 1
+                if index >= length:
+                    break
+            width = width // 2
+            print()
 
 
-def print_tree1(array):
-    '''
-    居中对齐方案, 逐个打印array中的元素
-    :param array:
-    :return:
-    '''
-    unit_width = len(str(array[-1]))  # 取最大元素的宽度作为单位宽度
-    length = len(array)
-    depth = math.ceil(math.log2(length + 1))  # 二叉树的深度（二叉树性质4）：具有n个结点的完全二叉树的深度为: math.ceil(math.log2(n))+1
-    index = 0
-    width = 2 ** depth - 1  # 满二叉树的宽度（作为第一行的宽度数居中打印元素）。
-    # 从上往下用投影的方法观察満二叉树时，各栅格（结点位置）互不重叠，且栅格数量等于满二叉树的结点总数。
-    # 满二叉树的结点数量为（二叉树性质2）: 2 ** depth(二叉树的深度) - 1
+    def print_tree2(array):
+        '''
+        投影栅格实现, 跳过array[0]中的元素
+        :param array:
+        :return:
+        '''
+        '''
+        计算空格单位数量。此处以深度为4的满二叉树为示例说明：
+        行  i  前空格       元素间空格（用于join函数来分隔行元素）
+        1   3  7=2*3-1     0=2*7+1  （第一行只有一个元素，所以元素间空格可为任意）
+        2   2  3=2*2-1     7=2*3+1
+        3   1  1=2*1-1     3=2*1+1
+        4   0  0=2*0-1     1=2*0+1 
+        '''
+        unit_width = len(str(max(array)))
+        # print('unit_width: ', unit_width)
+        length = len(array)
+        # print('Length : ', length)
+        index = 1
+        depth = math.ceil(math.log2(length))  # 二叉树的深度（二叉树性质4）：具有n个结点的完全二叉树的深度为: math.ceil(math.log2(n))+1，
+                                                # 因为接收的参数中已经添加一个占位元素，所以无需再加1，不然应该是math.ceil(math.log2(len(array)+1))
+        # print('Depth : ', depth)
+        sep = ' ' * unit_width
+        # print('Sep: ', sep)
 
-    for i in range(depth):  # 二叉树的行
-        for j in range(2 ** i):  # 计数：j来控制为每行元素的个数。0:1; 1:0,1; 2:0,1,2,3; 3:0,1,2,3,4,5,6,7
-            print('{:^{}}'.format(array[index], width * unit_width), end=' ' * unit_width)  # 居中打印元素，最后补充一个单位数空格
-            index += 1
-            if index >= length:
-                break
-        width = width // 2
-        print()
+        for i in range(depth - 1, -1, -1):  # 二叉树层数（迭代上方注释中的i）
+            pre = 2 ** i - 1  # 前空格数量
+            print(sep * pre, end='')  # 打印前空格
+            offset = 2 ** (depth - i - 1)
+            t_line = array[index: index + offset]  # 取元素
+            line = ['{:{}d}'.format(x, unit_width) for x in t_line]   # 将各元素所占用的长度位数统一为最长元素所占用的位数
+            index += offset
+            interval_space = sep * (2 * pre + 1)
+            print(interval_space.join(map(str, line)))
 
-
-def print_tree2(array):
-    '''
-    投影栅格实现, 跳过array[0]中的元素
-    :param array:
-    :return:
-    '''
-    '''
-    计算空格单位数量。此处以深度为4的满二叉树为示例说明：
-    行  i  前空格       元素间空格（用于join函数来分隔行元素）
-    1   3  7=2*3-1     0=2*7+1  （第一行只有一个元素，所以元素间空格可为任意）
-    2   2  3=2*2-1     7=2*3+1
-    3   1  1=2*1-1     3=2*1+1
-    4   0  0=2*0-1     1=2*0+1 
-    '''
-    unit_width = len(str(max(array)))
-    # print('unit_width: ', unit_width)
-    length = len(array)
-    # print('Length : ', length)
-    index = 1
-    depth = math.ceil(math.log2(length))  # 二叉树的深度（二叉树性质4）：具有n个结点的完全二叉树的深度为: math.ceil(math.log2(n))+1，
-                                            # 因为接收的参数中已经添加一个占位元素，所以无需再加1，不然应该是math.ceil(math.log2(len(array)+1))
-    # print('Depth : ', depth)
-    sep = ' ' * unit_width
-    # print('Sep: ', sep)
-
-    for i in range(depth - 1, -1, -1):  # 二叉树层数（迭代上方注释中的i）
-        pre = 2 ** i - 1  # 前空格数量
-        print(sep * pre, end='')  # 打印前空格
-        offset = 2 ** (depth - i - 1)
-        t_line = array[index: index + offset]  # 取元素
-        line = ['{:{}d}'.format(x, unit_width) for x in t_line]   # 将各元素所占用的长度位数统一为最长元素所占用的位数
-        index += offset
-        interval_space = sep * (2 * pre + 1)
-        print(interval_space.join(map(str, line)))
+    print_tree2(array) if supplement else print_tree1(array)
 
 
 if __name__ == '__main__':
     # print_tree1([x + 1 for x in range(10)])
     # print_tree1([30, 40, 50, 60, 70, 80, 90, 91, 93, 95, 97, 99, 100, 101, 102])
-    print_tree2([0, 30, 40, 50, 60, 70, 80, 90, 91, 93, 95, 97, 99, 100, 101, 102, 1021, 1023, 1024, 1025, 1026])
+    # print_tree2([0, 30, 40, 50, 60, 70, 80, 90, 91, 93, 95, 97, 99, 100, 101, 102, 1021, 1023, 1024, 1025, 1026])
     # print_tree2([0, 30, 40, 50, 60, 70, 80, 90, 91, 93, 95, 97, 99, 10, 10, 10])
+    print_tree([0, 30, 40, 50, 60, 70, 80, 90, 91, 93, 95, 97, 99, 100, 101, 102, 1021, 1023, 1024, 1025, 1026], True)
