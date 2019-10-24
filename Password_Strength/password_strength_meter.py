e4357553cba26f0758e35445fa90120c22cef0cd
Li Yinkai <yinkai.li@foxmail.com>
2019-10-24 22:58:31 +0800

    Complete password_strength_meter.py

--- Password_Strength/password_strength_meter.py
+++ Password_Strength/password_strength_meter.py
@@ -22,14 +22,17 @@ Date: 2019/10/20 下午9:34
 
 强度规定：
     强（Strong）: 符合所有组合规则， 例如: Aatb32_67mnq,其中包含大写字母、小写字母、数字和特殊字符,是合格的强密码。
-    一般（Average）： 符合三种组合规则
+    适中（Moderate）： 符合三种组合规则
     弱（Weak）： 符合两种及以下组合规则
 '''
 
-password1 = r'AbCd@135246_'
-password2 = r'AbCa5,'
 
 def strength_check(password : str) -> str:
+    '''
+    检查密码强度
+    :param password:
+    :return: 当密码合法时，返回密码强度，否则返回“Invalid Password”
+    '''
 
     # 检查密码长度
     def length_check(password = password, low = 10, upper = 15) -> bool:
@@ -58,18 +61,43 @@ def strength_check(password : str) -> str:
         # print(special_characters)
 
         regex = re.compile('[^a-zA-Z\d!%&#\-@_?*$+]+', re.S)
+        # regex = re.compile('[^a-zA-Z\d]+', re.S)
         illegal_characters = regex.findall(password)
         # print(illegal_characters)
         return False if illegal_characters else True
 
     # 检查字符组合
+    def get_grade(password = password) -> int:
+        '''
+        返回给定密码的组合数量
+        :param password:
+        :return: 返回密码的组合数量，0～4，0为没有合法字符
+        '''
+        grade = 0
+        upper_regex = re.compile('[A-Z]+', re.S)
+        lower_regex = re.compile('[a-z]+', re.S)
+        num_regex = re.compile('\d+', re.S)
+        special_regex = re.compile('[!%&#\-@_?*$+]+', re.S)
 
+        if special_regex.findall(password): grade += 1
+        if num_regex.findall(password): grade += 1
+        if upper_regex.findall(password): grade += 1
+        if lower_regex.findall(password): grade += 1
 
-    print(password)
-    print(illegal_check(password))
-    print(length_check(password))
+        return grade
 
+    if length_check(password) and illegal_check(password):
+        grade = get_grade(password)
+        if grade == 1: result = 'Weak'
+        elif grade == 2 or grade == 3: result = 'Moderate'
+        else: result = 'Strong'
+        return 'Password Strength: {}'.format(result)
+    else:
+        return 'Invalid Password!'
 
-
-
-strength_check(password2)
+if __name__ == '__main__':
+    password1 = r'AbCd@135246_'
+    password2 = r'AbCa@5,'
+    password3 = r'A'
+    password4 = r''
+    print(strength_check(password4))
