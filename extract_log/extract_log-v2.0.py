0a1fd27dacc3f910077e4800c3cd8c920819c131
Li Yinkai <yinkai.li@foxmail.com>
2019-11-05 22:46:26 +0800

    Amend extract_log-v2.0.py

--- extract_log/extract_log-v2.0.py
+++ extract_log/extract_log-v2.0.py
@@ -2,62 +2,39 @@
 import datetime
 import re
 
-def makekey(line:str, CHARS=set(""" []"'""")):
-    start = 0
-    length = len(line)
-    flag = False
-    for i, char in enumerate(line):
-        if char in CHARS:
-            if char == '[':
-                start = i + 1
-                flag = True
-            elif char == ']':
-                flag = False
-            elif char == '"':
-                flag = not flag
-                if flag: start = i + 1
-
-            if flag: continue
-
-            if start == i:
-                start = i + 1
-                continue
-
-            yield line[start:i]
-            start = i + 1
-
-        else:
-            if start == length:
-                result.append(line[start:i])
-
-
-names = ('remote', '', '', 'datetime', 'request', 'status', 'size', '', 'useragent')
-
-ops = (None, None, None,
-       lambda dstr: datetime.datetime.strptime(dstr, '%d/%b/%Y:%H:%M:%S %z'),
-       lambda request: dict(zip(['method', 'url', 'protocol'], request.split())),
-       int, int, None, None)
-
-# def extract(line:str):
-#     return dict(
-#         map(
-#             lambda triple: (triple[0], triple[1](triple[2]) if triple[1] else triple[2]),
-#             zip(names, ops, makekey(line))
-#         )
-#     )
+ops = {
+    'datetime':lambda dstr: datetime.datetime.strptime(dstr, '%d/%b/%Y:%H:%M:%S %z'),
+    'status': int, 'size':int
+}
 
 def extract(line:str):
-    pattern = r'([\d.]{7,15}) \S+ \S+ \[(.*)\]\s+"([^"]*)" (\d+) (\d+) \S+ "([^"]+)"'
+    pattern = r'(?P<remote>[\d.]{7,15}) \S+ \S+ \[(?P<datetime>.*)\]\s+"(?P<method>[^" ]*) (?P<url>[^" ]*) (?P<protocol>[^" ]*)" ' \
+              r'(?P<status>\d+) (?P<size>\d+) \S+ "(?P<useragent>[^"]+)"'
     regex = re.compile(pattern, re.S)
     matcher = regex.match(line)
     if matcher:
-        print(matcher)
-        print(line[matcher.start():matcher.end()])
-        print(matcher.groups())
+        # print(matcher)
+        # print(line[matcher.start():matcher.end()])
+        # print(matcher.groups())
+        # print(matcher.groupdict())
+        return {k:ops.get(k, lambda x:x)(v) for k, v in matcher.groupdict().items()}
+    else:
+        return line
+
+def load(filename:str):
+    with open(filename, encoding='utf8') as f:
+        for line in f:
+            fields = extract(line)
+            if isinstance(fields, (dict,)):
+                yield fields
+            else:
+                print('No match: {}'.format(fields))
 
 if __name__ == '__main__':
 
     line = '''114.249.235.230 - - [11/Apr/2017:10:49:51 +0800] "GET /path/to/file.py HTTP/1.1" 200 7635 "-" "Mozilla/5.0 (iPhone; CPU iPhone OS 10_2_1 like Mac OS X) AppleWebKit/602.4.6 (KHTML, like Gecko) Version/10.0 Mobile/14D27 Safari/602.1"'''
 
     # print(extract(line))
-    extract(line)
+    # extract(line)
+    for x in load('test.log'):
+        print(x)
