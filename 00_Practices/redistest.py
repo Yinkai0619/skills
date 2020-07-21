9fb8e2b390cd07d54421b2ca93e7ba828ca56572
Yinkai <yinkai.li@foxmail.com>
2020-07-21 23:41:41 +0800

    Add redistest.py

---
+++ 00_Practices/redistest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import redis
+import time
+
+pool = redis.ConnectionPool(host="localhost", port=6379, password="redispass", db=25)
+r = redis.Redis(connection_pool=pool)
+# r.flushdb
+for i in range(1000):
+    r.set("k%d" % i, "v%d" % i)
+    data=r.get("k%d" % i)
+    print(data)
+# print(r.keys)
