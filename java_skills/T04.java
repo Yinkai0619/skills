19d7819f9d00623f50cf6098c9cad4fc47644e9c
Yinkai <yinkai.li@foxmail.com>
2020-09-24 22:57:53 +0800

    Amend java_skills/T04.java

--- java_skills/T04.java
+++ java_skills/T04.java
@@ -21,12 +21,42 @@ public class T04 {
         //     num += 5;
         // }
 
-        int num = 1;
-        int sum = 1;
-        while( num <= 13 ) {
-            sum *= num;
-            num += 2;
+        // int num = 1;
+        // int sum = 1;
+        // while( num <= 13 ) {
+        //     sum *= num;
+        //     num += 2;
+        // }
+        /*
+        int sum = 0;
+        outer:for(int i = 1;i <= 100; i++){
+            sum += i;
+            System.out.println("Sum: " + sum);
+            while(sum >= 300){
+                break outer;
+            }
         }
-        System.out.println("Sum: " + sum);
+
+        int sum = 0;
+        outer:for(int i = 1; i <= 100; i++){
+            sum += i;
+            while(sum == 300){
+                continue outer;
+            }
+            System.out.println("Sum: " + sum);
+        }
+        */
+
+        int n = 0;
+        for(int i = 1; i <= 100; i++){
+            if(i%5==0){
+                System.out.print(i + "\t");
+                n++;
+                if(n%6==0){
+                    System.out.println();
+                }
+            }
+
+        }
     }
 }
