12be0b3d04f6fefed16a8df23fcbd09fdab8d838
Yinkai <yinkai.li@foxmail.com>
2020-10-29 23:20:51 +0800

    Add me.yinkai.2

---
+++ java_skills/TestJavaSE/TestOOP/src/me.yinkai.2/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String[] args) {
+        Person p1 = new Person("Nana",30,151.5, 95.4);
+        System.out.println(p1.name);
+        System.out.println(p1.age);
+        System.out.println(p1.height);
+        System.out.println(p1.weight);
+
+        Person p2 = new Person("Yinkai", 35, 172.1, 172.1);
+        System.out.println(p2.name);
+        System.out.println(p2.age);
+        System.out.println(p2.height);
+        System.out.println(p2.weight);
+
+    }
+}
