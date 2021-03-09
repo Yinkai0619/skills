0603d1c1ff59e5e5a883c820058b1cd49a6a6f1a
Li Yinkai <liyk@oimec.com.cn>
2021-03-09 13:01:41 +0800

    Amend zhandler

--- 00_Practices/ProcessAndThread/zlogging/zhandler.py
+++ 00_Practices/ProcessAndThread/zlogging/zhandler.py
@@ -1,18 +1,19 @@
 import logging
-
-logging.basicConfig(level=logging.ERROR, format="%(asctime)s\t\t%(message)s", datefmt="%Y/%m/%d %H:%M:%S")
+FORMAT = "%(asctime)s\t\t%(message)s"
+logging.basicConfig(level=logging.ERROR, format=FORMAT,
+                    datefmt="%Y/%m/%d %H:%M:%S")
 root = logging.getLogger()
 
 log1 = logging.getLogger("s")
 log1.setLevel(logging.INFO)
-log1.propagate=False
-log1.info("line1")
+log1.propagate = True
+# log1.info("line1")
 
-# handler = logging.FileHandler("/tmp/log1.txt", 'a')
-# log1.addHandler(handler)
+handler = logging.FileHandler("/tmp/log1.txt", 'a')
+log1.addHandler(handler)
 # log1.setLevel(logging.CRITICAL)
 #
-# log1.info("line2")
+log1.info("line2")
 # print(log1.handlers)
 
 # log2 = logging.getLogger("s.s1")
