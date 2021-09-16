6746c03737dd047e7472b66613080346a0f18d31
Li Yinkai <yinkai.li@foxmail.com>
2021-09-17 00:00:38 +0800

    add 00_Practices/db/

---
+++ 00_Practices/db/reader_v1.0.py
@@ -0,0 +1,37 @@
+import pymysql
+
+
+conn = None
+cursor = None
+
+try:
+    conn = pymysql.connect(host="localhost", user="pytest", password="pytestpass", database="test", port=33061)
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+    # insert_sql = "INSERT INTO user (login_name, first_name, last_name, password) VALUES ('user1', 'user', '1', sha('user1pass'))"
+    # cursor.execute(insert_sql)
+
+    user = "user12"
+    # pwd = "user12pass"
+    pwd = "12345' or 'a'='a"
+    sql = "SELECT * FROM user WHERE login_name='{}' and password = '{}'".format(user, pwd)
+    print(sql)
+    count = cursor.execute(sql)
+    # print(count)
+
+    # rows = cursor.fetchone()
+    # rows = cursor.fetchmany(2)
+    rows = cursor.fetchall()
+    print(rows)
+    print(cursor.rowcount, cursor.rownumber)
+
+    
+    # conn.commit()
+except Exception as e:
+    # conn.rollback()
+    print(e)
+finally:
+    if cursor:
+        cursor.close()
+    if conn:
+        conn.close()
