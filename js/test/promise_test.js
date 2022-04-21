2ff184a4001acc02d5387539989d960a6f6fd5a1
Li Yinkai <yinkai.li@foxmail.com>
2022-04-21 23:58:25 +0800

    add jses

--- js/test/promise_test.js
+++ js/test/promise_test.js
@@ -7,23 +7,39 @@ let p1 = new Promise(function (resolve, reject) {
     }, 3000);
 })
 
-// console.log("==========================")
-// console.log(p1)
+console.log("==========================")
+console.log(p1)
 
 // setTimeout(() => {
-//    console.log(p1) 
+//     console.log(p1)
 // }, 5000);
 
-p1.then(
-    value => console.log(1, value)
-    // ,
-    // reason => console.log(2, reason) 
-).then(
-    value => console.log(4, value)
-    ,
-    reason => console.log(5, reason) 
-)
+// p1.then(
+//     value => console.log(1, value)
+//     ,
+//     reeson => console.log(2, reason)
+// )
+
 
 // p1.catch(reason => console.log(3, reason))
+
+
+p1.then(                                // 返回新的Promise对象
+    value => {
+        console.log(1, value);      // fulfilled 状态
+        return Promise.reject(value + '!!!')
+    }
+    ,  
+    reason => {
+        console.log(2, reason); // rejected 状态
+        return Promise.reject(value + "~~~~~~")
+    }
+)
+// .then(
+//     value => console.log(4, value)
+//     ,
+//     reason => console.log(5, reason) 
+// )
+.catch(reason => console.log(3, reason))
 
 console.log('---------------------------------')
---
+++ js/test/src/moda.js
@@ -0,0 +1,19 @@
+
+export function foo() {
+    console.log("foo function")
+}
+
+// foo()
+
+
+// function * counter() {
+//     let i = 0;
+//     while(true) {
+//         yield ++i;
+//     }
+// }
+
+// g = counter();
+// console.log(g.next().value);
+// console.log(g.next().value);
+// console.log(g.next().value);
